e3284fc81bb0b85a9961c82d01f38d2183cd7003
marine <marine@mistgreen.net>
2012-12-23 19:57:22 +0900

    d3forumのコメント投稿欄ポップアップ機能を追加

--- html/common/fancybox/jQueryFancybox.4preload.js
+++ html/common/fancybox/jQueryFancybox.4preload.js
@@ -1,21 +1,22 @@
 jQuery(document).ready(function() {
+  jQuery("#d3f_post_form_quick_div").css("display","none");
 	jQuery('a[href$=".jpg"]').addClass("fancybox");
 	jQuery("a.fancybox").fancybox({
 		'overlayColor': '#000',
 		'overlayOpacity': 0.3,
 		'cyclic': true ,
-	    'transitionIn'  : 'elastic',
-	    'transitionOut' : 'elastic',
-	    'easingIn'      : 'easeOutBack',
-	    'easingOut'     : 'easeInBack',
-		'speedIn'		: 900 ,
-		'speedOut'		: 600 ,
+	  'transitionIn'  : 'elastic',
+	  'transitionOut' : 'elastic',
+	  'easingIn'      : 'easeOutBack',
+	  'easingOut'     : 'easeInBack',
+		'speedIn': 900 ,
+		'speedOut': 600 ,
 		'hideOnOverlayClick': false
 	});
 	jQuery("a.iframe").fancybox({
-        'width': '80%',
+    'width': '80%',
 		'height': '80%',
-        'autoScale': false,
+    'autoScale': false,
 		'transitionIn': 'none',
 		'transitionOut': 'none',
 		'type': 'iframe'
